ae74acfe14def91049844720d2c3d1ad319af014
HendsMede1r0s <hendersonbernardo2018@gmail.com>
2026-06-03 18:55:53 -0300

    feat: criação de classe PokemonModel

--- lib/main.dart
+++ lib/main.dart
@@ -23,3 +23,30 @@ class FavDexApp extends StatelessWidget {
     );
   }
 }
+
+
+class PokemonModel{
+  final int id; 
+  final String name;
+  final String? imageUrl;
+  final int? weight;
+  final int? height;
+
+  PokemonModel({
+    required this.id,
+    required this.name,
+    this.imageUrl,
+    this.weight,
+    this.height,
+  });
+
+  factory PokemonModel.fromJson(Map<String, dynamic> resultado){
+    return PokemonModel(
+      id: resultado['id'],
+      name: resultado['name'],
+      imageUrl: resultado['sprites']['front_default'],
+      weight: resultado['weight'],
+      height: resultado['height'],
+    );
+  }
+}
