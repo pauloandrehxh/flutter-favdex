8ab39f5ff5e6cc5b7e999a8fc8d1873bf5a39e5e
HendsMede1r0s <hendersonbernardo2018@gmail.com>
2026-06-25 09:54:32 -0300

    feat: status de modelos em detalhesPAge

--- lib/data/models/pokemon_model.dart
+++ lib/data/models/pokemon_model.dart
@@ -4,6 +4,13 @@ class PokemonModel {
   final String? imageUrl;
   final int? weight;
   final int? height;
+  final int statusBase;
+  final int hp;
+  final int attack;
+  final int defense;
+  final int spAttack;
+  final int spDefense;
+  final int speed;
 
   PokemonModel({
     required this.id,
@@ -11,9 +18,26 @@ class PokemonModel {
     this.imageUrl,
     this.weight,
     this.height,
+    required this.statusBase,
+    required this.hp,
+    required this.attack,
+    required this.spAttack,
+    required this.defense,
+    required this.spDefense,
+    required this.speed,
   });
 
   factory PokemonModel.fromJson(Map<String, dynamic> resultado) {
+
+    final listaStats = resultado['stats'] as List;
+    
+    final int hp = listaStats[0]['base_stat'];
+    final int attack = listaStats[1]['base_stat'];
+    final int defense = listaStats[2]['base_stat'];
+    final int spAttack = listaStats[3]['base_stat'];
+    final int spDefense = listaStats[4]['base_stat'];
+    final int speed = listaStats[5]['base_stat'];
+
     return PokemonModel(
       id: resultado['id'],
       name: resultado['name'],
@@ -21,6 +45,13 @@ class PokemonModel {
           resultado['sprites']['other']['official-artwork']['front_default'],
       weight: resultado['weight'],
       height: resultado['height'],
+      hp: hp,
+      attack: attack,
+      defense: defense,
+      spAttack: spAttack,
+      spDefense: spDefense,
+      speed: speed,
+      statusBase: hp + attack + defense + spDefense + speed + spAttack,
     );
   }
 }
